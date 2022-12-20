a64d00ccb576ec24cd763f7800071f8c65daff80
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2022-12-19 22:36:15 -0800

    code challenge 5

---
+++ javascript/linked-list/README.md
@@ -0,0 +1,34 @@
+# Singly Linked List
+
+Linked List Implementation and testing
+
+## Challenge
+
+> ### Node
+>
+> - Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
+>
+> ### Linked List
+>
+> - Create a Linked List class
+> - Within your Linked List class, include a head property.
+>   - Upon instantiation, an empty Linked List should be created.
+
+## Approach & Efficiency
+
+I participated in lecture in creating the demo code and referred to it to complete the assignment.
+
+## API
+
+> - insert
+>   - Arguments: value
+>   - Returns: nothing
+>   - Adds a new node with that value to the head of the list with an O(1) Time performance.
+> - includes
+>   - Arguments: value
+>   - Returns: Boolean
+>     - Indicates whether that value exists as a Node’s value somewhere within the list.
+> - to string
+>   - Arguments: none
+>   - Returns: a string representing all the values in the Linked List, formatted as:
+    `"{ a } -> { b } -> { c } -> NULL"`
--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,61 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../index');
 
 describe('Linked List', () => {
+  let linkedList;
+
   it('works', () => {
     expect(true).toBeTruthy();
   });
+
+  beforeEach(() => {
+    linkedList = new LinkedList();
+  });
+
+  it('can successfully instantiate an empty linked list', () => {
+    expect(linkedList.head).toBeNull();
+  });
+
+  it('can properly insert into the linked list', () => {
+    linkedList.insert(8);
+    expect(linkedList.head.value).toEqual(8);
+  });
+
+  it('the head property will properly point to the first node in the linked list', () => {
+    linkedList.insert(8);
+    expect(linkedList.head.value).toEqual(8);
+    linkedList.insert(11);
+    expect(linkedList.head.value).toEqual(11);
+  });
+
+  it('can properly insert multiple nodes into the linked list', () => {
+    linkedList.insert(8);
+    linkedList.insert(11);
+    linkedList.insert(7);
+    expect(linkedList.toString()).toEqual('{ 7 } -> { 11 } -> { 8 } -> NULL');
+  });
+
+  it('will return true when finding a value within the linked list that exists', () => {
+    linkedList.insert(8);
+    linkedList.insert(11);
+    linkedList.insert(7);
+    expect(linkedList.includes(8)).toBeTruthy();
+  });
+
+  it('will return false when searching for a value in the linked list that does not exist', () => {
+    linkedList.insert(8);
+    linkedList.insert(11);
+    linkedList.insert(7);
+    expect(linkedList.includes(10)).toBeFalsy();
+  });
+
+  it('can properly return a collection of all the values that exist in the linked list', () => {
+    linkedList.insert(8);
+    linkedList.insert(11);
+    linkedList.insert(7);
+    expect(linkedList.toString()).toEqual('{ 7 } -> { 11 } -> { 8 } -> NULL');
+  });
+
 });
